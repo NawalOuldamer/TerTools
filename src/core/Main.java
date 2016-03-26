28246396dc85cc1ab4487020cd8821483affa941
NawalOuldamer <nawal.ouldamer@gmail.com>
2016-03-26 16:01:23 +0100

    init

---
+++ src/core/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package core;
+
+/**
+ * @author ould-amer
+ * 15:59:38 26 mars 2016
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
